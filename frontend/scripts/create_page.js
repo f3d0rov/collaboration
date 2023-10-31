f397869d697348c81e3af94b553efa38f224cc92
Evan <evnfdrv@gmail.com>
2023-10-31 18:11:58 +0300

    page creation page layout complete

--- frontend/scripts/create_page.js
+++ frontend/scripts/create_page.js
@@ -5,8 +5,25 @@ function processTypeSwitch (newType) {
 
 	if (newType == "person") {
 		document.getElementById ("image").classList.remove ("square");
-	} else {
+		document.getElementById ('startDateName').innerHTML = "Дата рождения";
+		
+		document.getElementById ('aliveName').innerHTML = "Жив";
+		document.getElementById ('aliveBox').classList.remove ('hidden');
+		document.getElementById ('alive').dispatchEvent (new Event ('change'));
+	} else if (newType == "band") {
 		document.getElementById ("image").classList.add ("square");
+		document.getElementById ('startDateName').innerHTML = "Дата основания";
+		
+		document.getElementById ('aliveBox').classList.add ('hidden');
+		document.getElementById ('endDateName').classList.add ('hidden');
+		document.getElementById ('endDate').classList.add ('hidden');
+	} else /* newType == "album" */{
+		document.getElementById ("image").classList.add ("square");
+		document.getElementById ('startDateName').innerHTML = "Дата выпуска";
+
+		document.getElementById ('aliveBox').classList.add ('hidden');
+		document.getElementById ('endDateName').classList.add ('hidden');
+		document.getElementById ('endDate').classList.add ('hidden');
 	}
 }
 
@@ -40,6 +57,21 @@ function setupDataInput () {
 	if (params.has ("q")) {
 		document.getElementById ("name").value = params.get ("q");
 	}
+
+	let aliveCheckbox = document.getElementById ('alive');
+	aliveCheckbox.addEventListener (
+		'change',
+		() => {
+			if (aliveCheckbox.checked) {
+				document.getElementById ('endDate').classList.add ('hidden');
+				document.getElementById ('endDateName').classList.add ('hidden');
+			} else {
+				document.getElementById ('endDate').classList.remove ('hidden');
+				document.getElementById ('endDateName').classList.remove ('hidden');
+			}
+		}
+	);
+	aliveCheckbox.dispatchEvent (new Event('change'));
 }
 
 function setupPage () {
