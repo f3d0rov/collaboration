607021edd909cf4f40eb97f08f7d3021f9f98b49
Evan <evnfdrv@gmail.com>
2023-10-31 17:28:18 +0300

    page creation page progress - layout done?

--- frontend/scripts/create_page.js
+++ frontend/scripts/create_page.js
@@ -1,5 +1,16 @@
 
 
+function processTypeSwitch (newType) {
+	console.log (newType);
+
+	if (newType == "person") {
+		document.getElementById ("image").classList.remove ("square");
+	} else {
+		document.getElementById ("image").classList.add ("square");
+	}
+}
+
+
 function setupPageTypeSelector () {
 	let pageTypes = document.querySelectorAll ('.typeSelectorButton');
 	for (let i of pageTypes) {
@@ -9,13 +20,32 @@ function setupPageTypeSelector () {
 				let button = document.querySelector ('.typeSelectorButton.selected');
 				button.classList.remove ('selected');
 				ev.target.classList.add ('selected');
+				processTypeSwitch (ev.target.getAttribute('value'));
 			}
 		);
 	}
 }
 
+function setupImageInput () { 
+	document.getElementById ('imageBox').addEventListener (
+		'click',
+		(ev) => {
+			document.getElementById ('imageSelector').click();
+		}
+	)
+}
+
+function setupDataInput () {
+	let params = new URLSearchParams (window.location.search);
+	if (params.has ("q")) {
+		document.getElementById ("name").value = params.get ("q");
+	}
+}
+
 function setupPage () {
 	setupPageTypeSelector();
+	setupImageInput();
+	setupDataInput();
 }
 
 window.addEventListener (
